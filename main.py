466ee35cec3c365363c3f881327b2aa9352b2274
shushihao.ssh <shushihao.ssh@alibaba-inc.com>
2025-08-07 14:28:41 +0800

    training_policy_v4

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def get_args():
     parser.add_argument('--maxlen', default=101, type=int)
 
     # new params
-    parser.add_argument('--weight_decay', default=1e-5, type=float, help='Weight decay (AdamW)')
+    parser.add_argument('--weight_decay', default=1e-4, type=float, help='Weight decay (AdamW)')
     parser.add_argument('--warmup_steps', default=1000, type=int, help='Linear warmup steps')
     parser.add_argument('--clip_grad_norm', default=1.0, type=float, help='Max gradient norm for clipping')
     parser.add_argument('--use_adamw', action='store_true', help='Use AdamW instead of Adam')
@@ -123,7 +123,7 @@ if __name__ == '__main__':
             model.parameters(),
             lr=args.lr,
             betas=(0.9, 0.98),
-            weight_decay=args.weight_decay
+            # weight_decay=args.weight_decay
         )
 
     scheduler = LambdaLR(optimizer, lr_lambda)
@@ -168,8 +168,8 @@ if __name__ == '__main__':
             for param in model.item_emb.parameters():
                 loss += args.l2_emb * torch.norm(param)
             loss.backward()
-            # if args.clip_grad_norm > 0:
-            #     torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip_grad_norm)
+            if args.clip_grad_norm > 0:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip_grad_norm)
 
             optimizer.step()
             if global_step < args.warmup_steps:
